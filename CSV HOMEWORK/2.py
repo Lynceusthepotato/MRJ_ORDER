fef71531d7eb394d2e1e01e4cf09fd80ba1f554b
Lynceusthepotato <lukman.hanafi@outlook.com>
2019-10-13 16:35:48 +0700

    stuff i cant do
    im sorry my brain is super stupid

--- CSV HOMEWORK/2.py
+++ CSV HOMEWORK/2.py
@@ -7,7 +7,7 @@ import numpy as np
 import csv
 pd.plotting.register_matplotlib_converters()
 filename = "activity.csv"
-dates, steps = [], []
+dates, steps,intervals = [], [],[]
 mydict = {}
 with open(filename) as f:
     reader = csv.reader(f)
@@ -19,17 +19,12 @@ with open(filename) as f:
             date = datetime.strptime(row[1],"%Y-%m-%d")
             dates.append(date)
             interval = row[2]
+            intervals.append(interval)
     value = np.array(steps).astype(np.float)
-    datepo = np.array(dates)
-    bars = plt.bar(datepo, value, 1)
-    for bar in bars:
-        bar.set_edgecolor("black")
+    datepo = np.array(intervals).astype(np.float)
+    plt.plot(datepo,value)
     plt.ylabel("steps")
-    plt.xlabel("date")
+    plt.xlabel("intervals")
     plt.title("histogram")
-    plt.yticks([0, 100, 300, 500, 700, 800, 1000])
     plt.show()
-    means = np.mean(value) #i dont think this is correct
-    medians = np.median(value) #i know this is wrong
-    print(means,medians)
 
