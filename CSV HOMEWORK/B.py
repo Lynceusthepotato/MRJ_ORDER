9c5022761c8c853ecae02a56c99564a1fdf983f1
Lynceusthepotato <lukman.hanafi@outlook.com>
2019-10-14 13:49:43 +0700

    B (but i dont know if the 2. is right)

---
+++ CSV HOMEWORK/B.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from datetime import datetime
+import statistics
+import csv
+
+dicts = {}
+dictInterval = {}
+filename = "activity.csv"
+with open(filename) as f:
+    reader = csv.reader(f)
+    headrow = next(reader)
+    for row in reader:
+        steps = row[0]
+        if steps != "NA":
+            date = row[1]
+            stepss.append(steps)
+            date2 = int(datetime.strptime(date,"%Y-%m-%d").day)
+            interval = int(row[2])
+
+            dicts.setdefault(str(date),[])
+            dicts[str(date)].append(int(steps))
+
+            dictInterval.setdefault(interval,[])
+            dictInterval[interval].append(int(steps))
+
+meaninterval = [] #THIS IS THE ANSWER
+for i in dictInterval.keys(): # THIS IS THE ANSWER
+    meaninterval.append(statistics.mean(dictInterval.get(i)))
+plt.plot(meaninterval)
+plt.title("Histogram")
+plt.xlabel("5 min interval")
+plt.ylabel("average all day")
+plt.show()
+print("The max number of steps taken", max(meaninterval))
